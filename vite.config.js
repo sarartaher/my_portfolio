eeeb8b0561c90a6b7368aa056ca1531e2b2a0d3d
sarar2003 <saifurtahersarar7@gmail.com>
2025-11-22 23:30:00 +0600

    aam

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
-
-export default defineConfig(() => {
-  return {
-    base: "/", // Changed base path to root (/) for production deployment
-    plugins: [react()],
-  };
+export default defineConfig({
+  plugins: [react()],
+  base: './' // important for Vercel static deploy
 });
+
